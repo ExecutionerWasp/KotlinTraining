d7a81d2b86c47dd6fdb6417c3a7c5a012997bca2
ExecutionerWasp <ExecutionerWasp@gmail.com>
2021-07-23 15:46:50 +0300

    remove trash

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
 import requests.Mnemonic
 import requests.MonobankRequest
 import java.util.logging.Logger
-import java.util.stream.Collectors
 
 val log: Logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME)
 
@@ -12,23 +11,4 @@ fun main(args: Array<String>) {
 fun requestTraining() {
     println("EUR \n${MonobankRequest.CONNECT.getCurrency(Mnemonic.EUR)}")
     println("USD \n${MonobankRequest.CONNECT.getCurrency(Mnemonic.USD)}")
-}
-
-fun collectionTraining() {
-    val collection = ArrayList<Int>()
-    collection.add(1)
-    collection.add(2)
-    collection.add(3)
-    collection.add(4)
-    val result = collection
-        .stream()
-        .map { Value(it) }
-        .map { "${it.getValue()}" }
-        .collect(Collectors.joining(","))
-    log.info(result)
-    log.info("Sum ${collectionSum(collection)}")
-}
-
-fun collectionSum(collection: Collection<Int>): Int {
-    return collection.stream().mapToInt { it }.sum()
 }
