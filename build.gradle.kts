f40b1a2214c6a7c810a5f265994e58a3b2aabbfd
ExecutionerWasp <ExecutionerWasp@gmail.com>
2021-07-23 22:22:54 +0300

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.allopen") version "1.4.32"
+    kotlin("plugin.spring") version "1.3.72"
     java
     id("org.springframework.boot") version "2.5.3"
     application
